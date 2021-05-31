b346198e0c297bb6010e6a6f713b90a0a392a96b
Richard Germaine <54249860+rlgerma92@users.noreply.github.com>
2021-05-31 11:25:08 -0600

    more config

---
+++ android/app/src/main/kotlin/com/example/trich/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.trich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ios/trichWatch Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  trichWatch Extension
+//
+//  Created by Richard Germaine on 5/23/21.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
